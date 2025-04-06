19f5cd965c75c80d18f8d91cbc75182ef867e662
teviroff <mkaluzhnyy@mail.ru>
2025-04-06 23:54:59 +0300

    small tweaks

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,5 +1,14 @@
 from . import (
     auth,
+    # TODO: uncomment these, once all the errors are fixed
+    # geo,
+    # opportunity,
+    # user,
 )
 
-__all__ = ['auth']
+__all__ = [
+    'auth',
+    'geo',
+    'opportunity',
+    'user',
+]
--- endpoints/base.py
+++ endpoints/base.py
@@ -2,20 +2,27 @@ from typing import Annotated
 from random import choice
 
 from database.models.api import APIKey as _APIKey
+from database.models.pydantic_base import ObjectId
 import pydantic
 
 from config import app
 
+
 __all__ = ['app']
 
-OBJECT_ID_REGEX = r'^(\d[abcdef]){24}$'
-
 type APIKey = Annotated[str, pydantic.Field(pattern=_APIKey.API_KEY_REGEX)]
-type ID = Annotated[str, pydantic.Field(pattern=OBJECT_ID_REGEX)]
+type ID = ObjectId
 
 
 def generate_object_id() -> str:
     return ''.join([choice('abcdef0123456789') for i in range(24)])
+
+
+def escape_for_regex(string: str) -> str:
+    import re
+
+    return re.escape(string)
+
 
 class BaseQueryParams(pydantic.BaseModel):
     model_config = {
