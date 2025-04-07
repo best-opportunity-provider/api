2c3f63044de60adb34c0bad5f200c26805849cb2
teviroff <mkaluzhnyy@mail.ru>
2025-04-07 13:21:29 +0300

    added mongo auth to config

--- config.py
+++ config.py
@@ -22,6 +22,30 @@ logger = logging.getLogger()
 HOST: str = '127.0.0.1'
 PORT: int = 8001
 
-from fastapi import FastAPI
+from fastapi import FastAPI  # noqa: E402
 
 app = FastAPI()
+
+# ===== Database config =====
+
+from database import config as dbcfg  # noqa: E402
+
+
+def connect_mongo_db(
+    user: str, password: str, host: str, port: int, db_name: str, auth_db_name: str
+) -> None:
+    import mongoengine as mongo
+
+    mongo.connect(
+        host=f'mongodb://{user}:{password}@{host}:{port}/{db_name}?authSource={auth_db_name}'
+    )
+
+
+connect_mongo_db(
+    user=dbcfg.MONGO_USERNAME,
+    password=dbcfg.MONGO_PASSWORD,
+    host=dbcfg.MONGO_HOST,
+    port=dbcfg.MONGO_PORT,
+    db_name=dbcfg.MONGO_DB_NAME,
+    auth_db_name=dbcfg.MONGO_AUTH_DB_NAME,
+)
