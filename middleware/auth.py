cb90b042b162223f3e7bfb0db0871bcbe7b7d878
teviroff <mkaluzhnyy@mail.ru>
2025-04-07 16:29:15 +0300

    finished auth middleware

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,5 +1,4 @@
 from typing import Annotated
-from enum import IntEnum
 
 from fastapi import (
     Path,
@@ -10,39 +9,30 @@ from .base import APIKey as APIKeyModel
 from database import (
     APIKey,
     PersonalAPIKey,
+    DeveloperAPIKey,
 )
 import formatters as fmt
 
 
-class GetAPIKeyError(IntEnum):
-    INVALID_API_KEY = 0
-    INVALID_KEY_TYPE = 1
-
-
-ERRORS = {
-    'invalid_key': fmt.enum.Error(
-        type=200,
-        message=fmt.TranslatedString(
-            en='Provided API key is invalid',
-            ru='Предоставленный API-ключ недействителен',
-        ),
-        path=['query', 'api_key'],
-    ),
-    'invalid_key_type_personal': fmt.enum.Error(
-        type=201,
-        message=fmt.TranslatedString(
-            en='Provided API key must be personal',
-            ru='Предоставленный API-ключ должен быть персональным',
-        ),
-        path=['query', 'api_key'],
-    ),
-}
-
-get_personal_api_key_error_appender = fmt.enum.ErrorAppender[GetAPIKeyError](
+get_api_key_error_appender = fmt.enum.ErrorAppender[APIKey.GetErrorCode](
     transformer=fmt.enum.transformers.DictErrorTransformer(
         {
-            GetAPIKeyError.INVALID_API_KEY: ERRORS['invalid_key'],
-            GetAPIKeyError.INVALID_KEY_TYPE: ERRORS['invalid_key_type_personal'],
+            APIKey.GetErrorCode.INVALID_CATEGORY: fmt.enum.Error(
+                type=200,
+                message=fmt.TranslatedString(
+                    en='API keys of this category are not allowed for this method',
+                    ru='API-ключи этой категории не могут использоваться в этом методе',
+                ),
+                path=['query', 'api_key'],
+            ),
+            APIKey.GetErrorCode.INVALID_KEY: fmt.enum.Error(
+                type=201,
+                message=fmt.TranslatedString(
+                    en='Provided API key is invalid',
+                    ru='Предоставленный API-ключ недействителен',
+                ),
+                path=['query', 'api_key'],
+            ),
         }
     )
 )
@@ -52,14 +42,21 @@ async def get_personal_api_key(
     language: Annotated[fmt.Language, Path()],
     api_key: Annotated[APIKeyModel, Query()],
 ) -> PersonalAPIKey | fmt.ErrorTrace:
-    if (key := APIKey.get(api_key)) is not None:
-        if isinstance(key, PersonalAPIKey):
-            return key
+    key_or_error = APIKey.get(api_key, allowed_categories=[APIKey.Category.PERSONAL])
+    if isinstance(key_or_error, APIKey.GetErrorCode):
         errors = fmt.ErrorTrace()
-        get_personal_api_key_error_appender(
-            errors, GetAPIKeyError.INVALID_KEY_TYPE, language=language
-        )
+        get_api_key_error_appender(errors, key_or_error, language=language)
         return errors
-    errors = fmt.ErrorTrace()
-    get_personal_api_key_error_appender(errors, GetAPIKeyError.INVALID_API_KEY, language=language)
-    return errors
+    return key_or_error
+
+
+async def get_developer_api_key(
+    language: Annotated[fmt.Language, Path()],
+    api_key: Annotated[APIKeyModel, Query()],
+) -> DeveloperAPIKey | fmt.ErrorTrace:
+    key_or_error = APIKey.get(api_key, allowed_categories=[APIKey.Category.DEVELOPER])
+    if isinstance(key_or_error, APIKey.GetErrorCode):
+        errors = fmt.ErrorTrace()
+        get_api_key_error_appender(errors, key_or_error, language=language)
+        return errors
+    return key_or_error
